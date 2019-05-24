29e8dcacc20de91f8c9f33c48559d7fa15916254
Nehemia Litterat <nlitterat@gmail.com>
2019-05-24 14:29:13 +0300

    Improve example 4 structure

--- src/main/java/example4/ComposedBean.java
+++ src/main/java/example4/ComposedBean.java
@@ -3,6 +3,8 @@ package example4;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +12,6 @@ import org.springframework.stereotype.Component;
 @Scope("prototype")
 public class ComposedBean {
 	//@Autowired
-	//@Qualifier("proto6")
 	private PrototypeBean proto;
 	//@Autowired
 	private SingletonBean single;
